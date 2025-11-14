18acf3181d2616986030a5eb517045f96c2346e4
deepakselfhelp <154807257+deepakselfhelp@users.noreply.github.com>
2025-11-14 11:45:45 +0100

    Update webhook.js

--- api/razorpay/webhook.js
+++ api/razorpay/webhook.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     const subscription = body.payload?.subscription?.entity;
 
     console.log(`📬 Received Razorpay Event: ${event}`);
-   // console.log("🔍 Full Razorpay Body:", JSON.stringify(body, null, 2));
+    console.log("🔍 Full Razorpay Body:", JSON.stringify(body, null, 2));
 
     // 🗂️ Map internal Razorpay plan IDs to user-friendly names
     const PLAN_NAME_MAP = {
